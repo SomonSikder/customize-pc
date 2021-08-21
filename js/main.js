8d3274a4e8b42d561e6fb5674b3a39c23e480383
SomonSikder <somonsikder777@.gmail.com>
2021-08-21 18:58:26 +0600

    all done

--- js/main.js
+++ js/main.js
@@ -1,143 +1,77 @@
-// Function for Total Price without promo code
-function totalPriceUpdate(isAdd,extraCost){
-    const totalCost = document.getElementById('total-price')
-    const totalCostNumber = parseInt(totalCost.innerText)
-    if(isAdd){
-        return totalCost.innerText = totalCostNumber + extraCost
-    }
-    else{
-        return totalCost.innerText = totalCostNumber - extraCost
-    }
+//   founction for each even            
+function extraConfigurationCost(extraCostId,amount){
+    const extraProductText = document.getElementById(extraCostId);
+    extraProductText.innerText = amount;
+
+}
+
+function addTotal(){
+    const total= document.getElementById('total-price');
+    const bestPrice = document.getElementById("best-price").innerText;
+    const memoryCost8gb = document.getElementById("extra-memory").innerText;
+    const extraStorage = document.getElementById("extra-storage").innerText;
+    const deliveryCost = document.getElementById("delivery-cost").innerText;
+    const totalAmount= document.getElementById("total-amount");
+
+    total.innerText=parseInt(bestPrice)+parseInt(memoryCost8gb)+parseInt(extraStorage)+parseInt(deliveryCost);
+    totalAmount.innerText=parseInt(bestPrice)+parseInt(memoryCost8gb)+parseInt(extraStorage)+parseInt(deliveryCost);
     
 }
 
-
-// Function for Memory Cost
-function memoryCost(isCost){
-    const extraMemoryCost = document.getElementById('extra-memory-cost')
-    const extraMemoryCostText = parseInt(extraMemoryCost.innerText)
+// Memory event handler
+document.getElementById("memory-16gb").addEventListener('click',function(){
     
-    if(isCost){
-        const extraCost = extraMemoryCost.innerText = 180
-        totalPriceUpdate(true,extraCost)
-        PriceAfterPromo(true,extraCost)
-        
-    }else if(!isCost){
-        const extraCost = extraMemoryCost.innerText = 0
-        const totalUpdate = 180
-        totalPriceUpdate(false, totalUpdate)
-        PriceAfterPromo(false,totalUpdate)
-        
-    }
+    const memoryCost16gb = extraConfigurationCost('extra-memory',180);
+    addTotal();
     
-}
-const memory8GB = document.getElementById('memory-8gb')
-memory8GB.addEventListener('click',
-function(){
-    memoryCost(false)
-})
-const memory16GB = document.getElementById('memory-16gb')
-memory16GB.addEventListener('click',
-function(){
-    memoryCost(true)
-})
+}) 
 
-// Function For Extra Storage
-function storageCost(isCost){
-    const extraStorageCost = document.getElementById('extra-storage-cost')
-    const extraStorageCostText = parseInt(extraStorageCost.innerText)
-    if(isCost === 100){
-        const extraCost = extraStorageCost.innerText = 100
-        totalPriceUpdate(true, extraCost)
-        PriceAfterPromo(true,extraCost)
-    }
-    else if(isCost === 180){
-        const extraCost = extraStorageCost.innerText = 180
-        totalPriceUpdate(true, extraCost)
-        PriceAfterPromo(true, extraCost)
+document.getElementById('memory-8gb').addEventListener('click',function(){
+    
+    const memoryCost8gb = extraConfigurationCost('extra-memory',0);
+    addTotal();
+     
+})  
 
-    }
-    else{
-        const extraCost = extraStorageCost.innerText = 0
-        totalPriceUpdate(false, extraCost)
-        PriceAfterPromo(false,extraCost)
-        
-    }
-}
-
-const storage25Gb = document.getElementById('storage-256GB')
-const storage512Gb = document.getElementById('storage-512GB')
-const storage1Tb = document.getElementById('storage-1TB')
-storage25Gb.addEventListener('click',
-function(){
-    storageCost(0)
-})
-storage512Gb.addEventListener('click',
-function(){
-    storageCost(100)
-})
-storage1Tb.addEventListener('click',
-function(){
-    storageCost(180)
+// Storage event handler
+document.getElementById('storage-256gb').addEventListener('click',function(){
+   const storageCost256gb = extraConfigurationCost("extra-storage",0);
+   addTotal();
+  
 })
 
-
-// Function For Delivery Cost
-function deliveryCostUpdate(isCosting){
-    const deliveryCost = document.getElementById('delivery-cost')
-    const deliveryFreeText = parseInt(deliveryCost.innerText)
-    if(isCosting){   
-        const extraCost = deliveryCost.innerText = 20
-        totalPriceUpdate(true, extraCost)
-        PriceAfterPromo(true,extraCost)   
-    }
-    else if(!isCosting){
-        const extraCost = deliveryCost.innerText = 0
-        const totalUpdate = 20
-        totalPriceUpdate(false, totalUpdate)
-        PriceAfterPromo(false, totalUpdate)
-    }
-}
-
-const deliveryFree = document.getElementById('delivery-free')
-const deliveryCharged = document.getElementById('delivery-charged')
-
-deliveryFree.addEventListener('click', 
-function(){
-    deliveryCostUpdate(false)
+document.getElementById('storage-512gb').addEventListener('click',function(){
+    const storageCost512gb = extraConfigurationCost("extra-storage",100);
+    addTotal();
 })
 
-deliveryCharged.addEventListener('click', 
-function(){
-    deliveryCostUpdate(true)
+document.getElementById('storage-1tb').addEventListener('click',function(){
+    const storageCost1tb=extraConfigurationCost("extra-storage",180);
+    addTotal();
+})    
+
+//  Delivery event handler
+document.getElementById('delivery-free').addEventListener('click',function(){
+    const deliveryCost = extraConfigurationCost("delivery-cost",0);
+    addTotal();
+    
+}) 
+document.getElementById("delivey-charge").addEventListener('click',function(){
+    const deliveryCost = extraConfigurationCost("delivery-cost",20);
+    addTotal();
+    
 })
-
-
 
 // Promo code Apply part
-const applyBtn = document.getElementById('apply-btn')
-applyBtn.addEventListener('click',
-function(){
-    const inTotalPrice = document.getElementById('inTotal-price')
-    const inTotalNumber = parseInt(inTotalPrice.innerText)
-    
-    const promoCode = document.getElementById('promo-code')
-    const promoCodeValue = promoCode.value
-    if(promoCodeValue === 'stevekaku'){
-        inTotalPrice.innerText = inTotalNumber - inTotalNumber * 20/100
-        promoCode.value = ''
+document.getElementById('apply-btn').addEventListener('click',function(){
+    addTotal();
+    const totalText = document.getElementById("total-amount")
+    const totalPrice=parseInt(totalText.innerText)
+    const promoInput = document.getElementById('promo-code');
+    const promoInputValue=promoInput.value;
+    if(promoInputValue=="stevekaku"){
+        const amountDiscount=totalPrice - totalPrice*.2;
+        totalText.innerText=amountDiscount;
+        promoInput.value='';
     }
 })
-
-// Function for Total Price with promo code
-function PriceAfterPromo(isAdd, extraCost){
-    const inTotalPrice = document.getElementById('inTotal-price')
-    const inTotalPriceNumber = parseInt(inTotalPrice.innerText) 
-    if(isAdd){
-        return inTotalPrice.innerText = inTotalPriceNumber + extraCost
-    }
-    else{
-        return inTotalPrice.innerText = inTotalPriceNumber - extraCost
-    }
-}
-
